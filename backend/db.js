89264f3adae2b5de487b7f26c508f4350d232981
Amresh Singh Kushwah <69464490+AmreshSingh16@users.noreply.github.com>
2023-09-06 18:47:55 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://amresh164singh:BsniUunyA8Cqjn3E@cluster0.kjhtix0.mongodb.net/TryProjectsDB?retryWrites=true&w=majority"
+const mongoURI = ""
 const mongoDB = async()=>{
     await mongoose.connect(mongoURI  , {useNewUrlParser : true} ).then(function(err,result){
         const fetched_data =  mongoose.connection.db.collection("projects_datas");
